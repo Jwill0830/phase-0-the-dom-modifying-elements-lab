c0db67f56a0211c7246948cd7e95f637fae75e94
Jack Williams <jackmwilliams35@gmail.com>
2023-09-01 14:46:54 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+// Remove the DOM node with id="main"
+let mainNode = document.querySelector("main#main");
+if (mainNode) {
+  mainNode.remove();
+}
+
+// Create a new <h1> node and assign it to a variable "newHeader"
+let newHeader = document.createElement("h1");
+
+// Set the id of newHeader to "victory"
+newHeader.id = "victory";
+
+// Add text inside newHeader
+newHeader.innerHTML = "YOUR-NAME is the champion"; // Replace YOUR-NAME with your actual name
+
+// Append newHeader to the body
+document.body.appendChild(newHeader);
 // Write your code here!
